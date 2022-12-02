3a65aae5612b45a39d2db351e7d6ba7b35b59c0b
tluciad <tluciad@gmail.com>
2022-12-02 10:18:44 -0500

    assigned to a  parent class

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+    constructor(sqft, floors) {
+        super(sqft);
+
+        if (!Number.isInteger(floors)) {
+            throw TypeError('floors must be a number');
+          } else {
+            this._floors = floors;
+
+    } 
+}
+    get floors(){
+        return this._floors;
+    }
+    get sqft() {
+        return this._sqft;
+      }
+    //Override the method named evacuationWarningMessage  
+    evacuationWarningMessage() {
+        return ('Evacuate slowly the NUMBER_OF_FLOORS floors');
+    }
+}
