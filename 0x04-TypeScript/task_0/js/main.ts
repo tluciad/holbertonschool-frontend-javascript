7287069aa4e27ca8d2b06c4322c4a32513368737
tluciad <tluciad@gmail.com>
2022-12-08 10:10:24 -0500

    Creating an interface for a student

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,23 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+  }
+
+const student1: Student ={
+    firstName: "Pedro",
+    lastName: "Perez",
+    age: 30,
+    location: "Perú",
+}
+
+const student2: Student ={
+    firstName: "Maria",
+    lastName: "Rodriguez",
+    age: 28,
+    location: "Colombia",
+}
+const studentList = [student1, student2];
+
+
